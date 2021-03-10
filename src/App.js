d1b2fd767fd5a4988b53f97745ba3a360b997243
jaimish patel <pateljaimish629@gmail.com>
2021-03-09 17:14:23 -0800

    Addedd successfully

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React, {useState} from 'react';
+import Calculator from './Components/CalculatorFormJpa72/CalculatorFormJpa72'
 
 
 function App() {
@@ -15,20 +16,8 @@ function updateEquation(e){
 }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>This is calculator</h1>
+     <Calculator />
     </div>
   );
 }
